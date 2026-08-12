e5588b779b719e66c8de553ca0a4d627c60c867f
rrahul0904 <rrahul0904@users.noreply.github.com>
2026-08-12 15:08:13 -0400

    Boot into v23 certification guide home

--- frontend/app.js
+++ frontend/app.js
@@ -1,6 +1,6 @@
-import { renderNav } from "./components/nav.js?v=20260731-v21-editorial-replica";
+import { renderNav } from "./components/nav.js?v=20260812-v23-cert-guide";
 import { renderTopbar, refreshTopbar } from "./components/topbar.js?v=20260731-v21-editorial-replica";
-import { route } from "./router.js?v=20260731-v21-editorial-replica";
+import { route } from "./router.js?v=20260812-v23-cert-guide";
 
 async function boot() {
   await Promise.all([renderNav(), renderTopbar()]);
@@ -9,7 +9,7 @@ async function boot() {
     refreshTopbar();
     route();
   });
-  if (!window.location.hash) window.location.hash = "#/curriculum";
+  if (!window.location.hash) window.location.hash = "#/home";
   await route();
 }
 
